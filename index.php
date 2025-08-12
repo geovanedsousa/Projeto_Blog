f247b159ab60cc794e5c73849be3e2b4fddce428
Geovane Sousa <geovanedesousa203@gmail.com>
2025-08-12 12:59:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ include "./sistema/Nucleo/Mensagem.php";
 //$msg = new Mensagem();
 //echo $msg -> sucesso('Mensagem de sucesso')-> renderizar();
 
-
-
+echo (new Mensagem())-> erro ('Mensagem de erro')-> renderizar();
+echo '<hr>';
 
 
 ?>
