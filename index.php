94be966bf793eb9d18616d2404dfcbe526ebef65
Geovane Sousa <geovanedesousa203@gmail.com>
2025-08-14 23:27:42 -0300

    up

--- index.php
+++ index.php
@@ -5,13 +5,12 @@ require_once "sistema/configuracao.php";
 include_once "./sistema/Nucleo/helpers.php";
 include "./sistema/Nucleo/Mensagem.php";
 
-//$msg = new Mensagem();
-//echo $msg -> sucesso('Mensagem de sucesso')-> renderizar();
-//echo (new Mensagem())-> erro ('Mensagem de erro')-> renderizar();
 
-use sistema\Nucelo\Mensagem;
+//use sistema\Nucelo\Mensagem;
+//echo (new Mensagem)-> alerta('Mensagem de Alerta');
 
-echo (new Mensagem)-> alerta('Mensagem de Alerta');
+$hel = new Helpers();
+var_dump($hel-> validarCfp(57272934808));
 echo '<hr>';
 
 
