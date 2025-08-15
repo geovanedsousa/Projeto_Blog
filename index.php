0ecaca68fd1158a549b006f4f875dc9fa83d15c0
Geovane Sousa <geovanedesousa203@gmail.com>
2025-08-14 23:39:50 -0300

    up

--- index.php
+++ index.php
@@ -9,8 +9,9 @@ include "./sistema/Nucleo/Mensagem.php";
 //use sistema\Nucelo\Mensagem;
 //echo (new Mensagem)-> alerta('Mensagem de Alerta');
 
-$hel = new Helpers();
-var_dump($hel-> validarCfp(57272934808));
+echo Helpers:: saudacao();
+echo '<hr>'; 
+echo Helpers::dataAtual();
 echo '<hr>';
 
 
