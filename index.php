8bc726b521e4db78cb04e534a1305954e1770147
Geovane Sousa <geovanedesousa203@gmail.com>
2025-08-18 23:40:55 -0300

    up

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 //Arquivo index repomsável pela inicialização do sistema
 require 'vendor/autoload.php';
 
-echo sistema\Nucleo\Helpers::saudacao();
 
+$document = new \Bissolli\ValidadorCpfCnpj\CPF('123.456.789.00');
+
+var_dump($document->isValid());
 
 ?>
