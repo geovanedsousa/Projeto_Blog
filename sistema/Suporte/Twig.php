3cf3e9bc5eef203d78a41ad02315c0d07a33189e
Geovane Sousa <geovanedesousa203@gmail.com>
2025-08-29 23:21:56 -0300

    Twig Template

---
+++ sistema/Suporte/Twig.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace sistema\Suporte;
+
+class Twig
+{
+    private \Twig\Environment $twig;
+
+    public function __construct(string $diretorio)
+    {
+        $loader= new \Twig\Loader\FilesystemLoader($diretorio);
+        $this-> twig= new \Twig\Environment($loader);
+    }
+
+    public function renderizar(string $view, array $dados)
+    {
+        return $this->twig->render($view, $dados);
+    }
+
+}
+
+?>
